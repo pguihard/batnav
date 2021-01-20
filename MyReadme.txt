56ab2554dc09a4916e276527e87290257a19c838
pguihard <philippe.guihard@cegetel.net>
2021-01-20 19:32:18 +0100

    new implementation

---
+++ MyReadme.txt
@@ -0,0 +1,6 @@
+Server:
+PS G:\Workspace\html\test2> node server.js
+Server is running and listening on 8888 port.
+
+Client:
+http://localhost:8888
---
+++ server.js
@@ -0,0 +1,52 @@
+
+var io = require("socket.io");
+var express = require("express");
+var http = require("http");
+
+var app = express();
+var server = http.createServer(app);
+var socket = io(server);
+
+server.listen(8888);
+console.log("Server is running and listening on 8888 port.");
+
+app.use(express.static(__dirname + "/public"));
+app.get("/", function(req, res, next){
+    res.render("./public/index.html");
+});
+
+var allClients = 0;
+var clientId = 0;
+
+socket.on("connection", function(client) {
+    allClients += 1;
+    clientId += 1;
+
+    var my_timer;
+    var my_client = { "id": clientId, "obj": client };
+    
+    if (allClients > 2){
+        console.log("ClientID " + my_client.id + " is rejected.");
+        my_client.obj.send(JSON.stringify({ message: "disconnect"}));
+        allClients -= 1;
+        return;
+    }
+    console.log("ClientID " + my_client.id + " is connected.");
+    my_timer = setInterval(function () {
+        my_client.obj.send(JSON.stringify({ "timestamp": (new Date()).getTime(), "client": `ID: ${my_client.id}`, "clients": `${allClients} players`}));
+    }, 1000);
+    client.on("message", function(data) {
+        if (allClients < 2){
+            console.log("Nobody to receive your message.");
+            return;
+        }
+        //Send message to all clients except sender (broadcast)
+        my_client.obj.broadcast.send(JSON.stringify({ message: '"' + data + '" from ClientID ' + my_client.id }));
+        console.log("ClientID " + my_client.id + " sent: " + data);
+    });
+    client.on("disconnect", function() {
+        clearTimeout(my_timer);
+        allClients -= 1;
+        console.log("ClientID " + my_client.id + " is disconnected.");
+    });
+});
