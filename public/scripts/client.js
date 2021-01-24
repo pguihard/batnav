3f2e34cee99c9064e6833df921eabec18c7dfdc7
pguihard <philippe.guihard@cegetel.net>
2021-01-24 17:32:05 +0100

    2101241729

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,40 @@
+var createGrid=function(){
+    var tableString = "<table>";
+    for (row = 0; row < 10; row++) {
+        tableString += "<tr>";
+        for (col = 0; col < 10; col++) {
+            tableString += '<td id="' + row + col + '">O</td>';
+        }
+        tableString += "</tr>";
+    }
+    tableString += "</table>";
+    document.getElementById("board").innerHTML += tableString;
+};
+
+function clickOnBoard(event, sock) {
+    textClients = document.getElementById("clients").innerText;
+    if(textClients == "1 players" ){
+        return;
+    }
+    alert = document.getElementById("alert").innerText;
+    if(alert == "Please wait!" ){
+        return;
+    }
+    td = event.target.id
+    if (td == "board"){
+        return;
+    }
+    $("#alert").text("Please wait!");
+    document.getElementById(td).innerText = "X";
+    sock.send(td);
+}
+
 $(document).ready(function () {
+    createGrid();
     var sock = io();
+    document.getElementById("board").addEventListener("click", function(event) {
+        clickOnBoard(event, sock);
+      });
     sock.on("message", function (data) {
         var obj = JSON.parse(data);
         if (obj.message == "disconnect"){
@@ -25,15 +60,4 @@ $(document).ready(function () {
         }
     });
     sock.connect();
-    $("#poke").click(function() {
-        textClients = document.getElementById("clients").innerText;
-        if(textClients == "1 players" ){
-            return;
-        }
-        alert = document.getElementById("alert").innerText;
-        if(alert == "Please wait!" ){
-            return;
-        }
-        $("#alert").text("Please wait!");
-        sock.send(document.getElementById("tosend").value); });
 });
