7d759435fd5bfc98effb1090e6469d2b681e56a4
pguihard <philippe.guihard@cegetel.net>
2021-01-28 20:03:10 +0100

    2101282000

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,9 +6,9 @@ var msg1 = "Votre adversaire n'est pas connecté.";
 var msg2 = "Svp Attendez!";
 
 var shipId = 0; // 0-4
-// 5 ships [length, state, initial color], state: 0:intact 1:reached 2:sank
-var shipArea = [[5, 0, "#8ca78d"]] //, [4, 0, "#71ad73"], [3, 0, "#858f74"], [3, 0, "#7d9b48"], [2, 0, "#5c8b3c"]];
-//0:water, 1:reached, 2:sank, 3-7 ship Id
+// 5 ships [length, initial color]
+var shipArea = [[5, "#8ca78d"]]//, [4, "#71ad73"], [3, "#858f74"], [3, "#7d9b48"], [2, "#5c8b3c"]];
+//0:water, 1-5 ship Id, shipId x 10: reached, shipId x 10 + 1: sank
 var fleetArea = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],
                  [0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]];
 
@@ -54,8 +54,8 @@ function validateShip() {
             var id = "1" + row.toString() + col.toString();
             if (document.getElementById(id).textContent == "x") {
                 document.getElementById(id).textContent = "X";
-                document.getElementById(id).style.backgroundColor = shipArea[shipId][2];
-                fleetArea[row][col] = shipId+3; //0:water, 1:reached, 2:sank, 3-7 ship Id
+                document.getElementById(id).style.backgroundColor = shipArea[shipId][1];
+                fleetArea[row][col] = shipId+1; //0:water, 1-5 ship Id, ship Id x 10: reached, 99: sank
                 validated = true;
             }
         }
@@ -80,7 +80,7 @@ function clickOnBoard1(event) {
                 return;
             }
             document.getElementById(td).textContent = "x";
-            document.getElementById(td).style.backgroundColor = shipArea[shipId][2];
+            document.getElementById(td).style.backgroundColor = shipArea[shipId][1];
             var newval = parseInt(td.substring(1,2)) + 1;
             td = td.substring(0,1) + newval.toString() + td.substring(2,3);
             shipSize--;
@@ -94,7 +94,7 @@ function clickOnBoard1(event) {
                 return;
             }
             document.getElementById(td).textContent = "x";
-            document.getElementById(td).style.backgroundColor = shipArea[shipId][2];
+            document.getElementById(td).style.backgroundColor = shipArea[shipId][1];
             var newval = parseInt(td.substring(1,2)) - 1;
             td = td.substring(0,1) + newval.toString() + td.substring(2,3);
             shipSize--;
@@ -108,7 +108,7 @@ function clickOnBoard1(event) {
                 return;
             }
             document.getElementById(td).textContent = "x";
-            document.getElementById(td).style.backgroundColor = shipArea[shipId][2];
+            document.getElementById(td).style.backgroundColor = shipArea[shipId][1];
             var newval = parseInt(td.substring(2,3)) + 1;
             td = td.substring(0,1) + td.substring(1,2) + newval.toString();
             shipSize--;
@@ -122,7 +122,7 @@ function clickOnBoard1(event) {
                 return;
             }
             document.getElementById(td).textContent = "x";
-            document.getElementById(td).style.backgroundColor = shipArea[shipId][2];
+            document.getElementById(td).style.backgroundColor = shipArea[shipId][1];
             var newval = parseInt(td.substring(2,3)) - 1;
             td = td.substring(0,1) + td.substring(1,2) + newval.toString();
             shipSize--;
@@ -144,6 +144,7 @@ function validate(){
             lwest.style.display = "none"; west.style.display = "none";
             validation.style.display = "none";
             $("#alert").text(msg03);
+            console.log(fleetArea);
             createGrid("2"); initGrid("2");   
             comWithServer();
             return;
@@ -170,6 +171,18 @@ function clickOnBoard2(event, sock) {
     sock.send(td.substring(1));
 }
 
+function shipIsSank (ship_Id) {
+    for (var row = 0; row < 10; row++) {
+        for (var col = 0; col < 10; col++) {
+            if (fleetArea[row][col] == ship_Id) {
+                var id = "1" + row.toString() + col.toString();
+                document.getElementById(id).style.backgroundColor = "#f72929";
+                fleetArea[row][col] += 1;
+            }
+        }
+    }
+}
+
 function getTheShot(coord){
     var row = parseInt(coord.substring(0, 1));
     var col = parseInt(coord.substring(1, 2));
@@ -178,17 +191,16 @@ function getTheShot(coord){
         //into the water
         document.getElementById("1" + coord).style.backgroundColor = "#73B1B7";        
     }
-    else {
-        //reached
-        document.getElementById("1" + coord).style.backgroundColor = "#e78b8b";
-        if (--shipArea[ fleetArea[row][col] - 3][0] == 0) {
+    else if (fleetArea[row][col] < 10) {
+        if (--shipArea[ fleetArea[row][col] - 1][0] == 0) {
             //sank
-            document.getElementById("1" + coord).style.backgroundColor = "#f72929";            
+            fleetArea[row][col] *= 10;
+            shipIsSank(fleetArea[row][col]);
         }
         else {
             //just reached
             document.getElementById("1" + coord).style.backgroundColor = "#e78b8b";
-            
+            fleetArea[row][col] *= 10;
         }
     }
 }
